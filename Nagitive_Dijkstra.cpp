262b69f78c80c62baff7234908db45d4b5a56350
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-08-15 00:15:21 +0600

    module complete

---
+++ Nagitive_Dijkstra.cpp
@@ -0,0 +1,131 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+const int N = 1e5 + 5;
+const int INF = 1e9 + 10;
+vector<pii> adj[N];
+vector<int> dist(N, INF);
+vector<bool> visited(N);
+
+void dijkstra(int source)
+{
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+
+    dist[source] = 0;
+    pq.push({dist[source], source});
+
+    while (!pq.empty())
+    {
+        int u = pq.top().second;
+        pq.pop();
+
+        visited[u] = true;
+
+        for (pii vpair : adj[u])
+        {
+            int v = vpair.first;
+            int w = vpair.second;
+
+            if (visited[v])
+                continue;
+
+            // kaaj
+            if (dist[v] > dist[u] + w)
+            {
+                dist[v] = dist[u] + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    int n, m;
+    cin >> n >> m;
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+
+        adj[u].push_back({v, w}); // undirected
+
+        // adj[v].push_back({u, w}); // directed
+    }
+
+    // O(n+m)
+    dijkstra(1);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "Distance " << i << ": " << dist[i] << endl;
+    }
+
+    return 0;
+}
+/*
+
+
+*/
