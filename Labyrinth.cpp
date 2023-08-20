1b9b5fe5063efddb2c3d2095431a3ee76507380a
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-08-20 20:37:37 +0600

    Labyrinth Done

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -129,7 +129,52 @@ int main()
 
     bfs(si, sj);
 
-    cout << level[di][dj] << endl;
+    if (level[di][dj] != 0)
+    {
+        cout << "YES" << endl;
+        cout << level[di][dj] << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    vector<pii> path;
+    pii curr = {di, dj};
+    while (!(curr.first == si && curr.second == sj))
+    {
+        path.push_back(curr);
+        curr = parent[curr.first][curr.second];
+    }
+    path.push_back({si, sj});
+    reverse(path.begin(), path.end());
+    // for (auto p : path)
+    // {
+    //     cout << p.first << " " << p.second << endl;
+    // }
+
+    for (int i = 1; i < path.size(); i++)
+    {
+        // path[i-1] -> path[i]
+        if (path[i - 1].first == path[i].first)
+        {
+            if (path[i - 1].second == path[i].second - 1)
+            {
+                cout << "R";
+            }
+            else
+                cout << "L";
+        }
+        else
+        {
+            if (path[i - 1].first == path[i].first - 1)
+            {
+                cout << "D";
+            }
+            else
+                cout << "U";
+        }
+    }
 
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,12 +1,4 @@
-int curr = n;
-    // vector<int> path;
-    // while (curr != -1)
+for (auto p : path)
     // {
-    //     path.push_back(curr);
-    //     curr = parent[curr];
-    // }
-    // reverse(path.begin(), path.end());
-    // for (auto p : path)
-    // {
-    //     cout << p << " ";
+    //     cout << p.first << " " << p.second << endl;
     // }
