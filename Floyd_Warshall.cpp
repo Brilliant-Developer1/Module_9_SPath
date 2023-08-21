618552972e226b7afa5d4d71cc8e7a9dbeeab0f7
Brilliant-Developer1 <email2obaidul@gmail.com>
2023-08-21 23:36:13 +0600

    Floyd_Warshall

---
+++ Floyd_Warshall.cpp
@@ -0,0 +1,141 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+const int N = 1e3 + 7;
+const int INF = 1e9 + 7;
+int d[N][N];
+int n, m;
+
+void print_matrix()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (d[i][j] == INF)
+            {
+                cout << "X ";
+            }
+
+            else
+                cout << d[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+void dist_init()
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (i != j)
+            {
+
+                d[i][j] = INF;
+            }
+        }
+    }
+}
+
+int main()
+{
+
+    cin >> n >> m;
+
+    dist_init();
+
+    for (int i = 0; i < m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        d[u][v] = w;
+        // d[v][u] = w;
+    }
+
+    print_matrix();
+
+    for (int k = 1; k <= n; k++)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+            }
+        }
+    }
+    cout << "\nAfter\n";
+    print_matrix();
+
+    return 0;
+}
+
+/*
+5
+1 2 2
+1 3 6
+2 3 1
+3 4 4
+4 2 6
+2 5 3
+5 4 9
+*/
